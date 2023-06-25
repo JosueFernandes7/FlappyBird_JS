7884f468863bfc26e993747d55800efaa35913ec
Josué Fernandes <josue7mf@hotmail.com>
2023-06-24 22:02:59 -0300

    feat: adicionando movimentacao de pulo e queda

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,57 @@
-const gameBird = document.querySelector('.gameBird')
-let i = 0;
-window.addEventListener('click',(e) => {
-})
-let timer = null;
+const bird = document.querySelector('.gameBird')
+const gameBoard = document.querySelector('.gameBoard')
+
+const board = gameBoard.getBoundingClientRect()
+const birdSetting = bird.getBoundingClientRect()
+let posicaoAtual = bird.getBoundingClientRect().bottom
+let perdeu = false;
+
+let downTimer = null
+let timer = null
+let i = 0
+gameBoard.addEventListener('click', jump)
+function jump() {
+  clearInterval(downTimer)
+  const salto = 200 // Define a altura do salto
+
+  // Calcula a nova posição com base na posição atual e no salto
+  let novaPosicao = posicaoAtual + salto
+
+  if (novaPosicao >= board.height) {
+    novaPosicao = board.height - birdSetting.height/2
+  }
+  // Atualiza a posição do personagem
+  bird.style.bottom = novaPosicao + 'px'
+
+  // Atualiza a posição atual
+  posicaoAtual = novaPosicao
+  downMovement()
+}
+function downMovement() {
+  downTimer = setInterval(() => {
+    // Calcula a nova posição com base na posição atual na queda de 10px a cada 0.1s
+    let novaPosicao = parseFloat(posicaoAtual - 10);
+
+    if (novaPosicao <= 70) {
+      novaPosicao = 70;
+      perdeu = true;
+    }
+    // Atualiza a posição atual
+    posicaoAtual = novaPosicao;
+    // Atualiza a posição do personagem
+    bird.style.bottom = novaPosicao + "px"
+  }, 80)
+}
 function birdWingMovement() {
-    timer = setInterval(() => {
-      if(i % 2 == 0) {
-        gameBird.classList.add("birdUp")
-      i++;
+  timer = setInterval(() => {
+    bird.classList.remove(...["birdUp", "birdDown"])
+    if (i % 2 == 0) {
+      bird.classList.add("birdUp")
+      i++
     } else {
-      gameBird.classList.add("birdDown")
-      i--;
-      gameBird.classList.remove(...["birdUp","birdDown"])
+      bird.classList.add("birdDown")
+      i--
     }
   }, 300)
 }
+birdWingMovement()
